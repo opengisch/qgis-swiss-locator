9aa409eb970591fc290a04d11302140171f514ee
Germap <gcarrillo@linuxmail.org>
2024-10-12 03:54:23 -0500

    Update WMTS and location filter limits (#106)

--- swiss_locator/core/filters/swiss_locator_filter_location.py
+++ swiss_locator/core/filters/swiss_locator_filter_location.py
@@ -58,7 +58,7 @@ class SwissLocatorFilterLocation(SwissLocatorFilter):
         return "chs"
 
     def perform_fetch_results(self, search: str, feedback: QgsFeedback):
-        limit = self.settings.filter_location_limit.value()
+        limit = self.settings.filters[self.type.value]["limit"].value()
         url, params = map_geo_admin_url(
             search, self.type.value, self.crs, self.lang, limit
         )
