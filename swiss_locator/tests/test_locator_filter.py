a85257b015e59dda64b01142af1fca35af0fb6ee
Pi Moll <patricia@opengis.ch>
2025-08-18 15:07:53 +0200

    Add unit tests for local search

--- swiss_locator/tests/test_locator_filter.py
+++ swiss_locator/tests/test_locator_filter.py
@@ -15,13 +15,17 @@
 """
 
 from qgis.PyQt.QtTest import QSignalSpy
-
+from qgis._core import QgsStacExtent
+from qgis.core import QgsStacCollection, QgsLocator, QgsLocatorContext
 from qgis.testing import start_app, unittest
 from qgis.testing.mocked import get_iface
 
-from qgis.core import QgsLocator, QgsLocatorContext
-
-from swiss_locator.core.filters.swiss_locator_filter_wmts import SwissLocatorFilterWMTS
+from swiss_locator.core.filters.map_geo_admin_stac import \
+    collections_to_searchable_strings
+from swiss_locator.core.filters.swiss_locator_filter_stac import \
+    SwissLocatorFilterSTAC
+from swiss_locator.core.filters.swiss_locator_filter_wmts import \
+    SwissLocatorFilterWMTS
 
 start_app()
 
@@ -57,3 +61,51 @@ class TestSwissLocatorFilters(unittest.TestCase):
         spy.wait(1000)
 
         self.assertTrue(got_hit._results_[0].startswith("National Map"))
+
+
+class TestLocatorFilterSTAC(unittest.TestCase):
+    
+    @classmethod
+    def setUpClass(cls):
+        cls.test_strings = (
+            ('ch.swisstopo.swissalti3d', 'swissALTI3D'),
+            ('ch.bakom.mobilnetz-2g', '2G - GSM / EDGE Verfügbarkeit'),
+            ('ch.bafu.wald-wasserverfuegbarkeit_boden',
+                'Wasserverfügbarkeit im Boden (Standortwasserbilanz)'),
+        )
+        cls.collections = {}
+        for key, title in cls.test_strings:
+            cls.collections[key] = QgsStacCollection(key, None, None, [], None,
+                                                     QgsStacExtent())
+            cls.collections[key].setTitle(title)
+    
+    def setUp(self):
+        pass
+    
+    def test_collections_to_searchable_strings(self):
+        search_strings, search_ids = collections_to_searchable_strings(
+                self.collections)
+        
+        self.assertEqual(search_strings, [
+            'swissalti3d ch.swisstopo.swissalti3d',
+            '2g - gsm / edge verfügbarkeit ch.bakom.mobilnetz-2g',
+            'wasserverfügbarkeit im boden (standortwasserbilanz) ch.bafu.wald-wasserverfuegbarkeit_boden'
+        ])
+    
+    def test_local_search(self):
+        search_strings, search_ids = collections_to_searchable_strings(
+                self.collections)
+        
+        loc = QgsLocator()
+        _filter = SwissLocatorFilterSTAC(get_iface(), None,
+                                         [self.collections, search_strings,
+                                             search_ids])
+        loc.registerFilter(_filter)
+        
+        search_res_1 = _filter.perform_local_search('gsm')
+        search_res_2 = _filter.perform_local_search('verfügbarkeit')
+        
+        self.assertEqual(search_res_1, ['ch.bakom.mobilnetz-2g'])
+        self.assertEqual(search_res_2,
+                         ['ch.bafu.wald-wasserverfuegbarkeit_boden',
+                             'ch.bakom.mobilnetz-2g'])
